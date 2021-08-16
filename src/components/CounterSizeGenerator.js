80d9637baa381f275dd224d109d810281466dc6d
magluar <aryann.magluyan@oocl.com>
2021-08-16 20:04:23 +0800

    [counter][magluar] feat: add handleChangeSize for counterSizeGenerator

--- src/components/CounterSizeGenerator.js
+++ src/components/CounterSizeGenerator.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 function CounterSizeGenerator() {
+    const [size, setSize] = useState(1);
+
+    function handleChangeSize(event){
+        setSize(parseInt(event.target.value));
+    }
+
     return(
         <div>
             <span>Size:</span>
-            <input type="number" min="0"></input>
+            <input type="number" min="0" value={size} onChange={handleChangeSize}></input>
         </div>
     )
 }
